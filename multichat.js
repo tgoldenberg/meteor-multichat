771c19d257bc0801b775b2edfad305108232af02
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-21 20:54:47 -0400

    add chat form add user authentication to access chat form

--- multichat.js
+++ multichat.js
@@ -1,17 +1,10 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.chat.events({
+    'submit .new_chat_message': function(event) {
+      event.preventDefault();
+      var text = event.target.chat_message.value;
+      console.log(text);
+      document.getElementById("chat_message_field").value = "";
     }
   });
 }

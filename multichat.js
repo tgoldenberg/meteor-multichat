b6a9954c90b5ddaf2816ab88c68c2b0a83186b07
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-21 21:10:14 -0400

    add message collection and template

--- multichat.js
+++ multichat.js
@@ -1,4 +1,19 @@
+Messages = new Mongo.Collection("messages");
+if (Meteor.isServer) {
+  Meteor.publish("messages", function() {
+    return Messages.find();
+  });
+}
+
 if (Meteor.isClient) {
+  Meteor.subscribe("messages");
+
+  Template.chat.helpers({
+    messages: function() {
+      return Messages.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
   Template.chat.events({
     'submit .new_chat_message': function(event) {
       event.preventDefault();
@@ -7,10 +22,23 @@ if (Meteor.isClient) {
       document.getElementById("chat_message_field").value = "";
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addMessage: function(text) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Messages.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    })
+  }
+})
